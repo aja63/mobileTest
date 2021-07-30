20729aee5ca4f950c7a8b7ebccdd3f74f0b56564
aja63 <44000188+aja63@users.noreply.github.com>
2021-07-30 14:45:53 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,5 +1,7 @@
 document.getElementById("left").addEventListener("touchstart", press);
+document.getElementById("mid").addEventListener("touchstart", press);
+document.getElementById("right").addEventListener("touchstart", press);
 
 function press(){
-    alert('you pressed');
+    alert('you pressed' + this);
 }
