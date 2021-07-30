79b0fec715356f6b0bf2ce5e2ca45f5ac742ddf0
aja63 <44000188+aja63@users.noreply.github.com>
2021-07-30 15:09:58 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -4,7 +4,7 @@ document.getElementById("right").addEventListener("touchstart", press);
 
 function press(){
     switch(this.id){
-        case "left": alert(this.firstChild.style.left); break;
+        case "left": alert(this.firstChild); break;
         case "mid":  alert(this.firstChild.style.left); break;
         case "right": alert(this.firstChild.style.left); break;
     }
