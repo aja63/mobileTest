40c7d84d7ae5bd52a98ae6fd7eb8f3ef86f3daf8
aja63 <44000188+aja63@users.noreply.github.com>
2021-07-30 15:14:09 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,4 +1,4 @@
-document.getElementById("left").addEventListener("mousedown", press);
+document.getElementById("left").addEventListener("touchstart", press);
 document.getElementById("mid").addEventListener("touchstart", press);
 document.getElementById("right").addEventListener("touchstart", press);
 
